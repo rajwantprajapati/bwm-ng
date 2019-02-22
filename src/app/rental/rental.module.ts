59fd1ef757a012a721fd87f29219858162d242dd
Rajwant Prajapati <rajwantprajapati11@gmail.com>
2019-02-22 22:34:40 +0530

    Adds map component and google api

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { NgPipesModule } from 'ngx-pipes';
 
 import { RentalComponent } from './rental.component';
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
 import { RentalDetailComponent } from './rental-detail/rental-detail.component';
 import { UppercasePipe } from '../common/pipes/uppercase.pipe';
+import { MapModule } from '../common/map/map.module';
+import { NgPipesModule } from 'ngx-pipes';
 
 const routes: Routes = [
   { path: 'rentals',
@@ -25,7 +26,8 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forChild(routes),
     HttpClientModule,
-    NgPipesModule
+    NgPipesModule,
+    MapModule
   ],
   declarations: [
     RentalComponent,
