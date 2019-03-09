bcf221dd5b639d08054562f6bf4b983457e1b758
Rajwant Prajapati <rajwantprajapati11@gmail.com>
2019-03-09 16:26:01 +0530

    Adds booking functionality and rentals route imporvements

---
+++ server/routes/booking.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const BookingCtrl = require("../controllers/booking");
+const UserCtrl = require("../controllers/user");
+
+router.post("", UserCtrl.authMiddleWare, BookingCtrl.createBooking);
+
+module.exports = router;
