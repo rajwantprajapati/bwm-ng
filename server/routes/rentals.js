f218eef6dcaa3d5d141c1343c85c9362e12747a5
Rajwant Prajapati <rajwantprajapati11@gmail.com>
2019-03-11 22:16:44 +0530

    Adds endpoints for creation of rental and search rental

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -2,17 +2,11 @@ const express = require("express");
 const router = express.Router();
 const Rental = require("../models/rental");
 const UserCtrl = require("../controllers/user");
+const {normalizeErrors} = require("../helpers/mongoose");
+const User = require("../models/user");
 
 router.get('/secret', UserCtrl.authMiddleWare ,(req, res) => {
     res.json({"secret": true});
-});
-
-router.get('', (req, res) => {
-    Rental.find({})
-          .select('-bookings')
-          .exec((err, foundRentals) => {
-            res.json(foundRentals);
-          });    
 });
 
 router.get("/:id", (req, res) => {
@@ -27,6 +21,37 @@ router.get("/:id", (req, res) => {
             }
             return res.json(foundRental);
           })
+});
+
+router.get('', (req, res) => {
+    const city =  req.query.city;
+    const query = city ? {city: city.toLowerCase()} : {};
+    
+    Rental.find(query)
+        .select('-bookings')
+        .exec((err, foundRentals) => {
+        if(err) {
+            return res.status(422).send({errors: normalizeErrors(err.errors)});
+        }
+        if(city && foundRentals.length == 0) {
+            return res.status(422).send({errors: [{title: 'No Rentals Found!', detail: `There are no rentals for city ${city}`}]}); 
+        }
+        return res.json(foundRentals);
+        });
+});
+
+router.post('', UserCtrl.authMiddleWare, (req, res) => {
+    const {title, city, street, category, image, shared, bedrooms, description, dailyRate} = req.body;
+    const user = res.locals.user;
+    const rental = new Rental({title, city, street, category, image, shared, bedrooms, description, dailyRate});
+    rental.user = user;
+    Rental.create(rental, (err, newRental) => {
+        if(err) {
+            return res.status(422).send({errors: normalizeErrors(err.errors)});
+        }
+        User.updateOne({_id: user.id}, {$push: {rentals: newRental}}, ()=>{});
+        return res.json(newRental);
+    });
 });
 
 module.exports = router;
